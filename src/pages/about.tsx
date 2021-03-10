62e7efdfe90c1e985edfc58bc234bd663276531d
sitek94 <msitkowski94@gmail.com>
2021-03-10 20:07:14 +0100

    add graphql codegen

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -1,12 +1,29 @@
 import Layout from '../components/layout'
+import { graphql } from 'gatsby'
 
-export default function About() {
+import { AboutQuery } from '../../graphql-types'
+
+type AboutProps = {
+  data: AboutQuery
+}
+
+export default function About({ data }: AboutProps) {
   return (
     <Layout>
-      <h1>About me</h1>
+      <h1>{data?.site?.siteMetadata?.title}</h1>
       <p>
         I’m good enough, I’m smart enough, and gosh darn it, people like me!
       </p>
     </Layout>
   )
 }
+
+export const query = graphql`
+  query About {
+    site {
+      siteMetadata {
+        title
+      }
+    }
+  }
+`
