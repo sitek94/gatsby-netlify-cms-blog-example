e12f45960f301615a702d2aff88bbe0adeef7179
sitek94 <msitkowski94@gmail.com>
2021-03-14 20:25:36 +0100

    create default theme

---
+++ config/theme.ts
@@ -0,0 +1,24 @@
+const palette = {
+  mode: 'light',
+  primary: {
+    main: '#2c2c54',
+    light: '#474787',
+    contrastText: '#fff',
+  },
+  text: {
+    primary: 'rgba(0, 0, 0, 0.87)',
+  },
+  background: {
+    default: '#fff',
+  },
+  common: {
+    white: '#fff',
+    black: '#000',
+  },
+}
+
+const theme = {
+  palette,
+}
+
+export default theme
