611081f695378f9ffba0dc50b4a04f563e7c113c
sitek94 <msitkowski94@gmail.com>
2021-03-15 14:45:56 +0100

    accept `row` prop to change flex-direction

--- src/components/container.tsx
+++ src/components/container.tsx
@@ -2,11 +2,13 @@ import { css } from '@emotion/react'
 
 interface ContainerProps {
   maxWidth?: number
+  row?: boolean
   children: React.ReactNode
 }
 
 export default function Container({
   maxWidth = 720,
+  row,
   children,
 }: ContainerProps) {
   return (
@@ -15,6 +17,9 @@ export default function Container({
         width: 100%;
         max-width: ${maxWidth}px;
         margin: 0 auto;
+
+        display: flex;
+        flex-direction: ${row ? 'row' : 'column'};
       `}
     >
       {children}
