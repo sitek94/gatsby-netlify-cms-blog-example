3fa57e51e344f0dd61aa26409e0d21ea3cac42d5
sitek94 <msitkowski94@gmail.com>
2021-03-15 15:49:15 +0100

    add fonts, change primary color

--- config/theme.ts
+++ config/theme.ts
@@ -1,8 +1,12 @@
+import { fonts } from '../src/lib/typography'
+
 const palette = {
   mode: 'light',
   primary: {
-    main: '#2c2c54',
-    light: '#474787',
+    // main: '#2c2c54',
+    // light: '#474787',
+    light: '#393939',
+    main: '#000000de',
     contrastText: '#fff',
   },
   text: {
@@ -19,6 +23,7 @@ const palette = {
 
 const theme = {
   palette,
+  fonts,
 }
 
 export default theme
